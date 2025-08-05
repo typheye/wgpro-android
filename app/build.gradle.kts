94be6bce1fa57f9dcfca7f4ef2ea98b4552d0f2e
Typheye <78747175+typheye@users.noreply.github.com>
2025-08-05 20:29:32 +0800

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties().apply {
+    load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.typheye.wgpro"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.typheye.wgpro"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 2508050
+        versionName = "2.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file(keystoreProperties["release.store.file"] as String)
+            storePassword = keystoreProperties["release.store.password"] as String
+            keyAlias = keystoreProperties["release.key.alias"] as String
+            keyPassword = keystoreProperties["release.key.password"] as String
+        }
+
+        create("release") {
+            storeFile = file(keystoreProperties["release.store.file"] as String)
+            storePassword = keystoreProperties["release.store.password"] as String
+            keyAlias = keystoreProperties["release.key.alias"] as String
+            keyPassword = keystoreProperties["release.key.password"] as String
+        }
+    }
+
+    buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
